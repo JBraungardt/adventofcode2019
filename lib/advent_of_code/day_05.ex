72e9b9f34dd7cded676e555076e87c43f05b00fc
Juergen Braungardt <juergen.braungardt@mevis.de>
2019-12-09 17:57:13 +0100

    d5p2

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -14,8 +14,6 @@ defmodule AdventOfCode.Day05 do
       |> Integer.digits()
       |> pad_zeros()
 
-    # |> IO.inspect(label: "op")
-
     code =
       Enum.take(op, -2)
       |> Integer.undigits()
@@ -23,11 +21,34 @@ defmodule AdventOfCode.Day05 do
     modes = Enum.take(op, 3)
 
     case code do
-      99 -> input
-      1 -> add(input, pos, modes) |> process_position(pos + 4, input_value)
-      2 -> mult(input, pos, modes) |> process_position(pos + 4, input_value)
-      3 -> input(input, pos, modes, input_value) |> process_position(pos + 2, input_value)
-      4 -> output(input, pos, modes) |> process_position(pos + 2, input_value)
+      99 ->
+        input
+
+      1 ->
+        add(input, pos, modes) |> process_position(pos + 4, input_value)
+
+      2 ->
+        mult(input, pos, modes) |> process_position(pos + 4, input_value)
+
+      3 ->
+        input(input, pos, modes, input_value) |> process_position(pos + 2, input_value)
+
+      4 ->
+        output(input, pos, modes) |> process_position(pos + 2, input_value)
+
+      5 ->
+        next_pos = jump_true(input, pos, modes)
+        process_position(input, next_pos, input_value)
+
+      6 ->
+        next_pos = jump_false(input, pos, modes)
+        process_position(input, next_pos, input_value)
+
+      7 ->
+        less(input, pos, modes) |> process_position(pos + 4, input_value)
+
+      8 ->
+        equal(input, pos, modes) |> process_position(pos + 4, input_value)
     end
   end
 
@@ -75,6 +96,52 @@ defmodule AdventOfCode.Day05 do
     input
   end
 
+  defp jump_true(input, pos, modes) do
+    [_p3_mode, p2_mode, p1_mode] = modes
+
+    case get_value(input, pos + 1, p1_mode) do
+      0 ->
+        pos + 3
+
+      _ ->
+        get_value(input, pos + 2, p2_mode)
+    end
+  end
+
+  defp jump_false(input, pos, modes) do
+    [_p3_mode, p2_mode, p1_mode] = modes
+
+    case get_value(input, pos + 1, p1_mode) do
+      0 ->
+        get_value(input, pos + 2, p2_mode)
+
+      _ ->
+        pos + 3
+    end
+  end
+
+  defp less(input, pos, modes) do
+    [p3_mode, p2_mode, p1_mode] = modes
+    v1 = get_value(input, pos + 1, p1_mode)
+    v2 = get_value(input, pos + 2, p2_mode)
+
+    case v1 < v2 do
+      true -> write_value(input, 1, pos + 3, p3_mode)
+      false -> write_value(input, 0, pos + 3, p3_mode)
+    end
+  end
+
+  defp equal(input, pos, modes) do
+    [p3_mode, p2_mode, p1_mode] = modes
+    v1 = get_value(input, pos + 1, p1_mode)
+    v2 = get_value(input, pos + 2, p2_mode)
+
+    case v1 == v2 do
+      true -> write_value(input, 1, pos + 3, p3_mode)
+      false -> write_value(input, 0, pos + 3, p3_mode)
+    end
+  end
+
   defp pad_zeros(list) do
     for _ <- 1..(5 - length(list)) do
       0
@@ -82,6 +149,12 @@ defmodule AdventOfCode.Day05 do
     |> Kernel.++(list)
   end
 
-  def part2(args) do
+  def part2(args, input_value) do
+    Enum.with_index(args)
+    |> Enum.map(fn {value, pos} -> {pos, value} end)
+    |> Enum.into(%{})
+    |> process_position(0, input_value)
+
+    nil
   end
 end
