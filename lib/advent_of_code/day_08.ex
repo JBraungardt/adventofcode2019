392a59d7077c805815950fd7001df219d8d508e8
Juergen Braungardt <juergen.braungardt@gmx.de>
2019-12-27 22:46:00 +0100

    d8p1

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,5 +1,25 @@
 defmodule AdventOfCode.Day08 do
   def part1(args) do
+    layers =
+      String.split(args, "", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(150)
+
+    layer_index =
+      layers
+      |> Enum.map(&count_number(&1, 0))
+      |> Enum.with_index()
+      |> Enum.sort()
+      |> Enum.at(0)
+      |> elem(1)
+
+    layer = Enum.at(layers, layer_index)
+
+    count_number(layer, 1) * count_number(layer, 2)
+  end
+
+  defp count_number(layer, number) do
+    Enum.count(layer, &(&1 == number))
   end
 
   def part2(args) do
